a8dbffb94e4a384cc125e72508fe7ce05f83af57
Jacob Richardson <jacobr@groupon.com>
2012-03-12 12:32:01 -0500

    stringify date in ActiveRecordCache#retrieve

--- lib/active_record_cache.rb
+++ lib/active_record_cache.rb
@@ -12,7 +12,7 @@ class ActiveRecordCache
 
   def retrieve
     @repo.bug_fixes.map {|bug_fix| Bugwatch::BugFix.new(:sha => bug_fix.commit.sha, :file => bug_fix.file,
-                          :klass => bug_fix.klass, :function => bug_fix.function, :date => bug_fix.date_fixed) }
+                          :klass => bug_fix.klass, :function => bug_fix.function, :date => bug_fix.date_fixed.to_s) }
   end
 
 end
