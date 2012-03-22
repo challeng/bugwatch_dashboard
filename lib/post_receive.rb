11b4f63e0c5f21b8493e2ecdaaf5e653452e77e0
Jacob Richardson <jacobr@groupon.com>
2012-03-22 18:45:21 -0500

    add 1.8 compatibility for post_receive.rb

--- lib/post_receive.rb
+++ lib/post_receive.rb
@@ -1,48 +1,56 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
+$VERBOSE = nil
+
+require 'net/https'
 require 'rubygems'
 require 'json'
 
 class PostReceive
-  class << self
+ class << self
 
-    def payload(input)
-      old_revision, new_revision, ref = input.split(" ")
-      revisions = `git rev-list --first-parent #{new_revision}`
-      new_revisions = revisions.split("\n").take_while {|rev| rev != old_revision }.reverse
+   def payload(input)
+     old_revision, new_revision, ref = input.split(" ")
+     revisions = `git rev-list --first-parent #{new_revision}`
+     new_revisions = revisions.split("\n").take_while {|rev| rev != old_revision }.reverse
 
-      {
-          :ref => ref,
-          :repository => {:name => repo, :url => repo_url},
-          :commits => new_revisions.map{|rev| {:id => rev} }
-      }
-    end
+     {
+         :ref => ref,
+         :repository => {:name => repo, :url => repo_url},
+         :commits => new_revisions.map{|rev| {:id => rev} }
+     }
+   end
 
-    def post(input, hook_url)
-      uri = URI(hook_url)
-      Net::HTTP.post_form(uri, {:payload => JSON.dump(payload(input))})
-    end
+   def post(input, hook_url)
+     uri = URI(hook_url)
+     req = Net::HTTP::Post.new(uri.path)
+     req.set_form_data({:payload => JSON.dump(payload(input))})
 
-    private
+     http = Net::HTTP.new(uri.host, uri.port)
+     http.use_ssl = true
+     http.request(req)
+   end
 
-    def git_root
-      Dir.pwd.gsub(%r|\.git(/.*)?$|, '.git')
-    end
+   private
 
-    def repo
-      File.basename(git_root, ".git")
-    end
+   def git_root
+     Dir.pwd.gsub(%r|\.git(/.*)?$|, '.git')
+   end
 
-    def repo_url
-      "git:#{repo}.git"
-    end
+   def repo
+     File.basename(git_root, ".git")
+   end
 
-  end
+   def repo_url
+     "git:#{repo}.git"
+   end
+
+ end
 end
 
-if $0 == __FILE__
-  hook_url = nil
-  raise Exception, "need to supply a hook url" unless hook_url
-  PostReceive.post(STDIN.read, hook_url)
+
+if $0 == _FILE_
+ hook_url = nil
+ raise Exception, "need to supply a hook url" unless hook_url
+ PostReceive.post(STDIN.read, hook_url)
 end
