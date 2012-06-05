32a45f25edde2f544ad71d49c604c25d76a61e5b
Jacob Richardson <jacobr@groupon.com>
2012-06-05 12:26:27 -0600

    implement airbrake API in ExceptionSource

--- lib/exception_source.rb
+++ lib/exception_source.rb
@@ -1,7 +1,38 @@
+require 'nokogiri'
+
 class ExceptionSource
 
-  def self.deploy_before(sha)
-    # stub
+  class ExceptionMisconfiguration < Exception; end
+
+  def self.deploy_before(sha, opts={})
+    xml_response = self.get("/projects/1111/deploys.xml")
+    doc = Nokogiri::XML(xml_response)
+    sha_list = (doc / "scm-revision").map(&:text)
+    sha_list.drop_while {|deploy_sha| deploy_sha != sha }.drop(1).first
+  end
+
+  def self.get(path, options={}, &block)
+    full_path = base_uri + path
+    options[:query] ||= {}
+    options[:query][:api_key] = api_key
+    HTTParty.get(full_path, options, &block)
+  end
+
+  private
+
+  def self.base_uri
+    account = airbrake_config["account"]
+    "http://#{account}.airbrake.io" if account
+  end
+
+  def self.api_key
+    airbrake_config["api_key"]
+  end
+
+  def self.airbrake_config
+    config = AppConfig.exceptions || {}
+    raise ExceptionMisconfiguration, "You must configure an airbrake service as an exception source" unless config["airbrake"]
+    config["airbrake"]
   end
 
 end
--- test/unit/exception_source_test.rb
+++ test/unit/exception_source_test.rb
@@ -3,8 +3,67 @@ require 'exception_source'
 
 class ExceptionSourceTest < ActiveSupport::TestCase
 
-  test ".deploy_before exists" do
-    assert_nil ExceptionSource.deploy_before("sha")
+  PROJECT_ID = 1111
+  API_KEY = "api"
+  ACCOUNT = "test"
+  DEPLOY_XML = <<-XML
+<projects>
+  <deploy>
+    <id>123</id>
+    <project-id>#{PROJECT_ID}</project-id>
+    <rails-env>production</rails-env>
+    <created-at>2012-03-15 20:50:44 UTC</created-at>
+    <ends-at>2012-06-04 19:59:50 UTC</ends-at>
+    <scm-revision>sha</scm-revision>
+    <scm-repository>
+      git@git.domain.com:user/repository.git
+    </scm-repository>
+    <local-username>test_user</local-username>
+  </deploy>
+  <deploy>
+    <id>124</id>
+    <project-id>#{PROJECT_ID}</project-id>
+    <rails-env>production</rails-env>
+    <created-at>2012-03-15 20:50:44 UTC</created-at>
+    <ends-at>2012-06-04 19:59:50 UTC</ends-at>
+    <scm-revision>second_sha</scm-revision>
+    <scm-repository>
+      git@git.domain.com:user/repository.git
+    </scm-repository>
+    <local-username>test_user</local-username>
+  </deploy>
+  <deploy>
+    <id>125</id>
+    <project-id>#{PROJECT_ID}</project-id>
+    <rails-env>production</rails-env>
+    <created-at>2012-03-15 20:50:44 UTC</created-at>
+    <ends-at>2012-06-04 19:59:50 UTC</ends-at>
+    <scm-revision>third_sha</scm-revision>
+    <scm-repository>
+      git@git.domain.com:user/repository.git
+    </scm-repository>
+    <local-username>test_user</local-username>
+  </deploy>
+</projects>
+  XML
+
+  def setup
+    AppConfig.stubs(:exceptions).returns({"airbrake" => {"account" => ACCOUNT, "api_key" => API_KEY}})
+  end
+
+  test ".deploy_before gets deploys from airbrake" do
+    HTTParty.expects(:get).with("http://#{ACCOUNT}.airbrake.io/projects/#{PROJECT_ID}/deploys.xml", :query => {:api_key => API_KEY}).returns("")
+    ExceptionSource.deploy_before("sha", :project_id => PROJECT_ID)
+  end
+
+  test ".deploy_before raises exception if no airbrake configuration" do
+    AppConfig.stubs(:exceptions).returns(nil)
+    assert_raises(ExceptionSource::ExceptionMisconfiguration) { ExceptionSource.deploy_before("sha") }
+  end
+
+  test ".deploy_before returns deploy sha after given sha" do
+    HTTParty.expects(:get).returns(DEPLOY_XML)
+    assert_equal "second_sha", ExceptionSource.deploy_before("sha")
   end
 
 end
