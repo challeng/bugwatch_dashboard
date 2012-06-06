82e85fef44b5052a48519f8759911952b7996c76
Jacob Richardson <jacobr@groupon.com>
2012-06-06 13:24:35 -0600

    update DefectHook to use GET instead of POST

--- lib/hooks/defect_hook.rb
+++ lib/hooks/defect_hook.rb
@@ -2,9 +2,10 @@ require "sinatra/base"
 
 class DefectHook < Sinatra::Base
 
-  post '/defect' do
-    defect_params = params['payload']
-    priority, title, ticket_id = defect_params.split("|")
+  get '/defect' do
+    priority = params['priority']
+    title = params['title']
+    ticket_id = params['id']
     Defect.create!(:priority => priority, :title => title, :ticket_id => ticket_id)
     "OK"
   end
