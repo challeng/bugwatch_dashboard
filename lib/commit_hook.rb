ae27aba156f0b4c99fc07cf21cfb2cc26a79816f
Jacob Richardson <jacobr@groupon.com>
2012-06-13 10:07:35 -0600

    validate input in CommitHook

--- lib/commit_hook.rb
+++ lib/commit_hook.rb
@@ -6,7 +6,7 @@ class CommitHook < Sinatra::Base
     payload = JSON.parse(params['payload'])
     repository = payload['repository']
     commits = payload['commits']
-    if payload['ref'] == "refs/heads/master"
+    if valid_data?(repository, commits) && payload['ref'] == "refs/heads/master"
       commits.each do |commit|
         Resque::Job.create(repository['name'].to_sym, CommitAnalysisWorker,
                            repository['name'], repository['url'], commit['id'])
@@ -14,4 +14,27 @@ class CommitHook < Sinatra::Base
     end
     "OK"
   end
+
+  private
+
+  def valid_data?(repository_data, commits)
+    alphanumeric?(repository_data['name']) &&
+        commits.all? {|commit_data| sha? commit_data['id'] } &&
+        url?(repository_data['url'])
+  end
+
+  def alphanumeric?(data)
+    data.match(/^[A-Za-z0-9_-]+$/)
+  end
+
+  def sha?(data)
+    data.match(/^[a-z0-9]+$/)
+  end
+
+  def url?(data)
+    URI.parse(data)
+  rescue URI::InvalidURIError
+    data.match(/^git\@git\:[A-Za-z0-9_-]+\.git/)
+  end
+
 end
--- test/unit/commit_hook_test.rb
+++ test/unit/commit_hook_test.rb
@@ -19,24 +19,54 @@ class CommitHookTests < Test::Unit::TestCase
   end
 
   test "POST /hook enqueues analysis worker for commit" do
-    commit_params = [{:id => 'XXX'}]
+    commit_params = [{:id => 'xxx'}]
     Resque::Job.expects(:create).
-        with(repo_params[:name].to_sym, CommitAnalysisWorker, repo_params[:name], repo_params[:url], 'XXX')
+        with(repo_params[:name].to_sym, CommitAnalysisWorker, repo_params[:name], repo_params[:url], 'xxx')
     post '/hook', get_payload(commit_params)
   end
 
   test "POST /hook enqueues multiple commits" do
-    commit_params = [{:id => 'XXX'}, {:id => 'ZZZ'}]
+    commit_params = [{:id => 'xxx'}, {:id => 'zzz'}]
     Resque::Job.expects(:create).
-        with(repo_params[:name].to_sym, CommitAnalysisWorker, repo_params[:name], repo_params[:url], 'XXX')
+        with(repo_params[:name].to_sym, CommitAnalysisWorker, repo_params[:name], repo_params[:url], 'xxx')
     Resque::Job.expects(:create).
-        with(repo_params[:name].to_sym, CommitAnalysisWorker, repo_params[:name], repo_params[:url], 'ZZZ')
+        with(repo_params[:name].to_sym, CommitAnalysisWorker, repo_params[:name], repo_params[:url], 'zzz')
     post '/hook', get_payload(commit_params)
   end
 
   test "POST /hook does not enqueue if ref is not master" do
-    payload = get_payload([:id => 'XXX'], repo_params, "refs/heads/not_master")
+    payload = get_payload([:id => 'xxx'], repo_params, "refs/heads/not_master")
     Resque::Job.expects(:create).never
+    post '/hook', payload
+  end
+
+  test "POST /hook does not enqueue if name not valid" do
+    payload = get_payload([:id => 'xxx'], repo_params.merge(:name => "sudo rm -rf"))
+    Resque::Job.expects(:create).never
+    post '/hook', payload
+  end
+
+  test "POST /hook does not enqueue if url not valid" do
+    payload = get_payload([:id => 'xxx'], repo_params.merge(:url => "sudo rm -rf"))
+    Resque::Job.expects(:create).never
+    post '/hook', payload
+  end
+
+  test "POST /hook does not enqueue if sha not valid" do
+    payload = get_payload([:id => 'sudo rm -rf'])
+    Resque::Job.expects(:create).never
+    post '/hook', payload
+  end
+
+  test "POST /hook does not enqueue if any shas are not valid" do
+    payload = get_payload([:id => 'xxx', :id => 'invalid sha'])
+    Resque::Job.expects(:create).never
+    post '/hook', payload
+  end
+
+  test "POST /hook enqueues worker if url is a proxy and not uri" do
+    payload = get_payload([:id => 'xxx'], repo_params.merge(:url => "git@git:test_repo.git"))
+    Resque::Job.expects(:create)
     post '/hook', payload
   end
 
