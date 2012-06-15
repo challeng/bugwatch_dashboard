89a5d688de5e758804bf25792ef6b0f7c221764b
Jacob Richardson <jacobr@groupon.com>
2012-06-14 19:32:47 -0600

    call ZendeskService when receiving data through ZendeskHook

--- lib/hooks/zendesk_hook.rb
+++ lib/hooks/zendesk_hook.rb
@@ -3,10 +3,16 @@ require "sinatra/base"
 class ZendeskHook < Sinatra::Base
 
   get '/zendesk' do
-    priority = params['priority']
-    title = params['title']
-    ticket_id = params['id']
-    ZendeskDefect.create!(:priority => priority, :title => title, :ticket_id => ticket_id)
+    begin
+      priority = params['priority']
+      title = params['title']
+      ticket_id = params['id']
+      status = params['status']
+      secret = params['secret']
+      ZendeskService.activity(priority: priority, title: title, id: ticket_id, status: status, secret: secret)
+    rescue Exception => e
+      Rails.logger.error e
+    end
     "OK"
   end
 
--- test/hooks/zendesk_hook_test.rb
+++ test/hooks/zendesk_hook_test.rb
@@ -3,12 +3,22 @@ require 'rack_test_helper'
 class ZendeskHookTest < Test::Unit::TestCase
   include RackTest
 
-  test "GET /zendesk creates defect" do
+  test "GET /zendesk calls zendesk service" do
     priority = "Urgent"
     title = "Cannot create something"
     ticket_id = "12345"
-    ZendeskDefect.expects(:create!).with(:priority => priority, :title => title, :ticket_id => ticket_id)
-    get '/zendesk', {:priority => priority, :title => title, :id => ticket_id}
+    status = "Urgent"
+    secret = "bugwatch"
+    params = {priority: priority, title: title, id: ticket_id, status: status, secret: secret}
+    ZendeskService.expects(:activity).with(params)
+    get '/zendesk', params
+  end
+
+  test "GET /zendesk logs exception" do
+    ZendeskService.expects(:activity).raises(Exception)
+    Rails.logger.expects(:error)
+    get '/zendesk', {}
+    assert_equal last_response.body, "OK"
   end
 
 end
