5ea37f6d308aed3cf7ab0d7a88e53a62fbc7738c
Jacob Richardson <jacobr@groupon.com>
2012-07-13 12:08:43 -0600

    associate repo to release when importing

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,10 +1,18 @@
+require 'exception_source'
+
 class Release < ActiveRecord::Base
   belongs_to :repo
 
+  scope :production, where(:env => "production")
+
   def self.update!(project_id)
+    repo_name, _ = ExceptionSourceConfig.repo_name_and_config_by_project_id(project_id)
+    repo = Repo.find_by_name! repo_name
     ExceptionSource.deploys(project_id).each do |deploy|
-      Release.find_or_create_by_sha(deploy[:sha], deploy_date: deploy[:date], env: deploy[:env])
+      Release.find_or_create_by_deploy_date_and_repo_id(deploy[:date], repo.id, sha: deploy[:sha], env: deploy[:env])
     end
+  rescue ActiveRecord::RecordNotFound
+    nil
   end
 
 end
--- test/unit/release_test.rb
+++ test/unit/release_test.rb
@@ -2,13 +2,26 @@ require 'test_helper'
 
 class ReleaseTest < ActiveSupport::TestCase
 
-  test ".update! finds or creates release by sha" do
-    project_id = "1234"
+  attr_reader :project_id
+
+  def setup
+    @project_id = "1234"
+  end
+
+  test ".update! finds or creates release by deploy date and repo" do
     deploy_sha = "sha"
     deploy_date = "2010-10-10"
     deploy_env = "production"
+    repo = repos(:test_repo)
+    ExceptionSourceConfig.expects(:repo_name_and_config_by_project_id).with(project_id).returns([repo.name, {}])
     ExceptionSource.expects(:deploys).with(project_id).returns([{sha: deploy_sha, date: deploy_date, env: deploy_env}])
-    Release.expects(:find_or_create_by_sha).with(deploy_sha, deploy_date: deploy_date, env: deploy_env)
+    Release.expects(:find_or_create_by_deploy_date_and_repo_id).with(deploy_date, repo.id, sha: deploy_sha, env: deploy_env)
+    Release.update!(project_id)
+  end
+
+  test ".update! does not update releases if repo not found" do
+    ExceptionSourceConfig.expects(:repo_name_and_config_by_project_id).with(project_id).returns(["not repo name", {}])
+    ExceptionSource.expects(:deploys).never
     Release.update!(project_id)
   end
 
