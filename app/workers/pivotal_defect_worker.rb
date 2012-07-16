1e03441e3e87bdbdc9d2a008a4a70843fa3639d5
Jacob Richardson <jacobr@groupon.com>
2012-07-16 09:51:43 -0600

    add logging to defect workers

--- app/workers/pivotal_defect_worker.rb
+++ app/workers/pivotal_defect_worker.rb
@@ -3,10 +3,14 @@ require 'config/pivotal_config'
 class PivotalDefectWorker
 
   def self.perform(pivotal_project_id, activity_data)
-    repo_name, _ = PivotalConfig.repo_name_and_token_by_project_id(pivotal_project_id)
-    exception_source_project_id = ExceptionSourceConfig.project_id_by_repo_name(repo_name)
-    Release.update! exception_source_project_id
-    PivotalService.activity activity_data
+    begin
+      repo_name, _ = PivotalConfig.repo_name_and_token_by_project_id(pivotal_project_id)
+      exception_source_project_id = ExceptionSourceConfig.project_id_by_repo_name(repo_name)
+      Release.update! exception_source_project_id
+      PivotalService.activity activity_data
+    rescue => e
+      Rails.logger.error e
+    end
   end
 
 end
--- app/workers/zendesk_defect_worker.rb
+++ app/workers/zendesk_defect_worker.rb
@@ -3,10 +3,14 @@ class ZendeskDefectWorker
   class << self
 
     def perform(secret, defect_data)
-      repo_name, _ = ZendeskConfig.repo_config_by_secret(secret)
-      project_id = ExceptionSourceConfig.project_id_by_repo_name(repo_name)
-      Release.update!(project_id)
-      ZendeskService.activity(defect_data)
+      begin
+        repo_name, _ = ZendeskConfig.repo_config_by_secret(secret)
+        project_id = ExceptionSourceConfig.project_id_by_repo_name(repo_name)
+        Release.update!(project_id)
+        ZendeskService.activity(defect_data)
+      rescue => e
+        Rails.logger.error e
+      end
     end
 
   end
